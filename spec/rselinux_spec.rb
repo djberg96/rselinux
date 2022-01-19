85a5765648892837b55492fb53dca0fe4cab1c9d
Daniel Berger <djberg96@gmail.com>
2022-01-19 09:01:00 -0500

    Add more path specs.

--- spec/rselinux_spec.rb
+++ spec/rselinux_spec.rb
@@ -32,5 +32,25 @@ RSpec.describe Linux::SELinux do
       expect(described_class.current_policy_path).to be_kind_of(String)
       expect(described_class.current_policy_path).to end_with('policy')
     end
+
+    example "customizable_types_path returns expected value" do
+      expect(described_class.customizable_types_path).to be_kind_of(String)
+      expect(described_class.customizable_types_path).to end_with('customizable_types')
+    end
+
+    example "default_context_path returns expected value" do
+      expect(described_class.default_context_path).to be_kind_of(String)
+      expect(described_class.default_context_path).to end_with('default_contexts')
+    end
+
+    example "default_type_path returns expected value" do
+      expect(described_class.default_type_path).to be_kind_of(String)
+      expect(described_class.default_type_path).to end_with('default_type')
+    end
+
+    example "failsafe_context_path returns expected value" do
+      expect(described_class.failsafe_context_path).to be_kind_of(String)
+      expect(described_class.failsafe_context_path).to end_with('failsafe_context')
+    end
   end
 end
