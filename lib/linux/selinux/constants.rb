84b9b6a649b1c791df98f46f406e0d42470e2ec3
Daniel Berger <djberg96@gmail.com>
2022-02-09 08:08:02 -0500

    Add label constants.

---
+++ lib/linux/selinux/constants.rb
@@ -0,0 +1,21 @@
+module Linux
+  module SELinux
+    module Constants
+      module LabelConstants
+        SELABEL_CTX_FILE = 0
+        SELABEL_CTX_MEDIA = 1
+        SELABEL_CTX_X = 2
+        SELABEL_CTX_DB = 3
+        SELABEL_CTX_ANDROID_PROP = 4
+        SELABEL_CTX_ANDROID_SERVICE = 5
+        SELABEL_OPT_UNUSED = 0
+        SELABEL_OPT_VALIDATE = 1
+        SELABEL_OPT_BASEONLY = 2
+        SELABEL_OPT_PATH = 3
+        SELABEL_OPT_SUBSET = 4
+        SELABEL_OPT_DIGEST = 5
+        SELABEL_NOPT = 6
+      end
+    end
+  end
+end
